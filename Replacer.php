c6592df01575d3bdf9abf07b689a9b8c058c79fd
Tõnis Ormisson <tonis@andmemasin.eu>
2016-09-28 20:09:38 +0300

    DEV add Replacer

---
+++ Replacer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace andmemasin\helpers;
+
+/**
+ * Replacement values helper
+ *
+ * @package app\models\helpers
+ * @author Tonis Ormisson <tonis@andmemasin.eu>
+ */
+class Replacer {
+    /**
+     * Replace the {values} by $params[] values in $text
+     * @param string $text
+     * @param array[] $params
+     * @return string
+     */
+    public static function replace($text, $params = []) {
+        $defaultParams = ReplacementValue::getParams();
+        $allParams = array_merge($params,$defaultParams);
+        return preg_replace_callback('/{([^}]+)}/', function ($m) use ($allParams) {
+            // skip if is not set
+            if(isset($allParams[$m[1]])){
+                return $allParams[$m[1]];
+            }
+            return false;
+            }, $text);
+    }
+}
