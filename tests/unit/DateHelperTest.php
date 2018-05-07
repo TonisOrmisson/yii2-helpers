791afba834dca8837bef035c36b9fea6f1b16287
Tõnis Ormisson <tonis@andmemasin.eu>
2018-05-07 12:15:02 +0300

    DEV: add helpers

---
+++ tests/unit/DateHelperTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace andmemasin\helpers;
+
+class DateHelperTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \andmemasin\helpers\UnitTester
+     */
+    protected $tester;
+
+
+
+    public function testHasTimeReached()
+    {
+        $this->assertTrue(true);
+    }
+    
+
+}
