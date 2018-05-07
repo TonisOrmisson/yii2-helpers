058c7e956b2ff4a6ca75ffc3b96c73585ffe6101
Tõnis Ormisson <tonis@andmemasin.eu>
2018-05-07 13:25:53 +0300

    DEV: display limesurvey errors

--- tests/unit/DateHelperTest.php
+++ tests/unit/DateHelperTest.php
@@ -8,12 +8,36 @@ class DateHelperTest extends \Codeception\Test\Unit
      */
     protected $tester;
 
+    const TIME_REACHED = 1;
+    const TIME_NOT_REACHED = 2;
 
 
-    public function testHasTimeReached()
+
+    public function additionProvider()
     {
-        $this->assertTrue(true);
+        return [
+            [self::TIME_NOT_REACHED, '2001-01-02 01:01:01'],
+        ];
     }
-    
+
+
+
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testHasTimeReached($timeType,$time)
+    {
+        $nowTime = "2001-01-01 01:01:01";
+        $stub = $this->createMock('\DateTime');
+        $stub->expects($this->any())
+            ->will($this->returnValue($nowTime));
+        if ($timeType === self::TIME_REACHED) {
+            $this->assertTrue(DateHelper::hasTimeReached($time));
+        } else {
+            $this->assertFalse(DateHelper::hasTimeReached($time));
+        }
+
+    }
+
 
 }
