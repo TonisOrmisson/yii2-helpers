e631e36406d3d2e7694af6d86852cee3ca3165df
Tõnis Ormisson <tonis@andmemasin.eu>
2018-05-07 13:58:48 +0300

    DEV: helper tests

--- DateHelper.php
+++ DateHelper.php
@@ -14,12 +14,24 @@ class DateHelper {
     /** @var string End of time for DB logical delete. */
     const END_OF_TIME = '3000-12-31 00:00:00.000000';
 
+    /** @var DateTime */
+    public $now;
+
+
+    public function __construct()
+    {
+        $now = new DateTime();
+    }
+
 
     /**
      * Get current time in mysql datetime(6) format as "Y-m-d H:i:s.u"
      * @return string
+     * @deprecated use dynamic
      */
     public static function getDatetime6(){
+
+        // TODO this does now consider the getNow()
         $t = microtime(true);
         $micro = sprintf("%06d",($t - floor($t)) * 1000000);
         $d = new DateTime( date('Y-m-d H:i:s.'.$micro, $t) );
@@ -32,6 +44,7 @@ class DateHelper {
      * End of time is a default value for time not in reach to mark
      * date where we will not reach. Typically default for time_closed value
      * @return string
+     * @deprecated use dynamic
      */
     public static function getEndOfTime(){
         return self::END_OF_TIME;
@@ -40,6 +53,7 @@ class DateHelper {
     /**
      * @param string $datetime
      * @return false|string
+     * @deprecated use dynamic
      */
     public static function getMysqlDateTimeFromDateTime6 ($datetime) {
         return date("Y-m-d H:i:s", strtotime( $datetime ));
@@ -49,6 +63,7 @@ class DateHelper {
     /**
      * @param string  $dateTime
      * @return false|string
+     * @deprecated use dynamic
      */
     public static function getDatetimeForDisplay($dateTime){
         return date("Y-m-d H:i:s", strtotime( $dateTime ));
@@ -57,9 +72,10 @@ class DateHelper {
     /**
      * @param string $sqlDate
      * @return mixed
+     * @deprecated use dynamic
      */
     public static function getDateDifferenceInDays($sqlDate){
-        $dStart  = new DateTime();
+        $dStart  = self::getNow();
         $dEnd = new DateTime($sqlDate);
         $dDiff = $dStart->diff($dEnd);
         $days = $dDiff->days;
@@ -70,8 +86,17 @@ class DateHelper {
      * @param string $dateTime time that we want to check whether it has reached or not
      * @return bool
      */
-    public static function hasTimeReached($dateTime)
+    public function hasTimeReached($dateTime)
     {
-        return new \DateTime() >= new \DateTime($dateTime);
+        return $this->now >= new \DateTime($dateTime);
     }
+
+    /**
+     * @return DateTime
+     * @deprecated use dynamic
+     */
+    public static function getNow() {
+        return new \DateTime();
+    }
+
 }
--- tests/unit/DateHelperTest.php
+++ tests/unit/DateHelperTest.php
@@ -16,25 +16,42 @@ class DateHelperTest extends \Codeception\Test\Unit
     public function additionProvider()
     {
         return [
+            [self::TIME_NOT_REACHED, '2001-01-01 01:01:02'],
+            [self::TIME_NOT_REACHED, '2001-01-01 01:02:01'],
+            [self::TIME_NOT_REACHED, '2001-01-01 02:01:01'],
             [self::TIME_NOT_REACHED, '2001-01-02 01:01:01'],
+            [self::TIME_NOT_REACHED, '2001-02-01 01:01:01'],
+            [self::TIME_NOT_REACHED, '2002-01-02 01:01:01'],
+            [self::TIME_NOT_REACHED, '2021-01-01 01:01:01'],
+            [self::TIME_NOT_REACHED, '2201-01-01 01:01:01'],
+            [self::TIME_NOT_REACHED, '3001-01-01 01:01:01'],
+
+            [self::TIME_REACHED, '2001-01-01 01:01:01'], // spot on
+            [self::TIME_REACHED, '2001-01-01 01:01:00'],
+            [self::TIME_REACHED, '2001-01-01 01:00:01'],
+            [self::TIME_REACHED, '2001-01-01 00:01:01'],
+            [self::TIME_REACHED, '2001-01-00 01:01:01'],
+            [self::TIME_REACHED, '2001-00-01 01:01:01'],
+            [self::TIME_REACHED, '2000-01-01 01:01:01'],
+            [self::TIME_REACHED, '1001-01-01 01:01:01'],
         ];
     }
 
 
-
     /**
      * @dataProvider additionProvider
+     * @param int $timeType
+     * @param string $time
      */
     public function testHasTimeReached($timeType,$time)
     {
-        $nowTime = "2001-01-01 01:01:01";
-        $stub = $this->createMock('\DateTime');
-        $stub->expects($this->any())
-            ->will($this->returnValue($nowTime));
+        $nowTime = new \DateTime("2001-01-01 01:01:01");
+        $dateHelper = new DateHelper();
+        $dateHelper->now = $nowTime;
         if ($timeType === self::TIME_REACHED) {
-            $this->assertTrue(DateHelper::hasTimeReached($time));
+            $this->assertTrue($dateHelper->hasTimeReached($time));
         } else {
-            $this->assertFalse(DateHelper::hasTimeReached($time));
+            $this->assertFalse($dateHelper->hasTimeReached($time));
         }
 
     }
